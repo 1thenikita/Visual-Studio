be6ad21033853a20ce6169107cb564ddf164d15e
BuildTools <unconfigured@null.spigotmc.org>
2020-12-17 19:14:09 +0300

    Version change

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DiscordRPforVS.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -74,18 +74,6 @@ namespace DiscordRPforVS.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool resetTimestamp {
-            get {
-                return ((bool)(this["resetTimestamp"]));
-            }
-            set {
-                this["resetTimestamp"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool largeLanguage {
             get {
                 return ((bool)(this["largeLanguage"]));
@@ -128,6 +116,18 @@ namespace DiscordRPforVS.Properties {
             }
             set {
                 this["translates"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool resetTimestamp {
+            get {
+                return ((bool)(this["resetTimestamp"]));
+            }
+            set {
+                this["resetTimestamp"] = value;
             }
         }
     }
