8bbb2909e267de14308bb63ce00aedf3f8466f0a
0xb4dc0d3 <69420353+0xb4dc0d3x@users.noreply.github.com>
2021-10-19 13:00:07 +0330

    2022 Preview

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,5 @@
 ﻿# Changelog
 
-## [v5.5.4] (26-09-2021)
-###### Fixed error message "bad exception"!
-
-**SQUASHED BUGS**
-- Fixed error message "bad exception"!
-
 ## [v5.5.3] (24-09-2021)
 ###### Fixed bugs and added a switch to English!
 
--- Constants.cs
+++ Constants.cs
@@ -8,10 +8,11 @@
     {
         public static readonly Dictionary<String[], String[]> Languages = new Dictionary<String[], String[]>
         {
-            { new String[] { ".H", ".C", ".CC", ".HH", ".CPP", ".IPP", ".INL", ".C++", ".H++", ".HPP" }, new String[] { "cpp", "C++" } },
+            { new String[] { ".H", ".C" }, new String[] { "c", "C" } },
+            { new String[] { ".CC", ".HH", ".CPP", ".IPP", ".INL", ".C++", ".H++", ".HPP" }, new String[] { "cpp", "C++" } },
             { new String[] { ".GO" }, new String[] { "go", "GO" } },
             { new String[] { ".PHP" }, new String[] { "php", "PHP" } },
-            { new String[] { ".RB", ".rbw" }, new String[] { "ruby", "Ruby" } },
+            { new String[] { ".RB", ".RBW" }, new String[] { "ruby", "Ruby" } },
             { new String[] { ".CS" }, new String[] { "csharp", "C#" } },
             { new String[] { ".FS", ".FSI", ".FSX", ".FSSCRIPT" }, new String[] { "fsharp", "F#" } },
             { new String[] { ".TS" }, new String[] { "typescript", "Typescript" } },
@@ -24,22 +25,23 @@
             { new String[] { ".LESS" }, new String[] { "less", "LESS" } },
             { new String[] { ".HTML" }, new String[] { "html", "Html" } },
             { new String[] { ".JS" }, new String[] { "javascript", "Javascript" } },
-            { new String[] { "CMAKELISTS.TXT", "CMAKECACHE.TXT" }, new String[] { "cmake", "CMake" } },
+            { new String[] { "CMAKELISTS.TXT", "CMAKECACHE.TXT", ".CMAKE" }, new String[] { "cmake", "CMake" } },
             { new String[] { ".MD", ".MARKDOWN" }, new String[] { "markdown", "Markdown" } },
             { new String[] { ".XML" }, new String[] { "xml", "XML" } },
             { new String[] { ".XAML" }, new String[] { "xaml", "XAML" } },
-            { new String[]{ ".CSHTML", ".RAZOR" }, new String[] { "cshtml", "CSHtml" } },
-            { new String[]{ ".RS" }, new String[] { "rust", "Rust" } },
-            { new String[]{ ".TOML" }, new String[] { "toml", "TOML" } },
-            { new String[]{ ".LUA" }, new String[] { "lua", "Lua" } },
-            { new String[]{ ".BAS", ".CLS", ".FRM", ".VBP", ".VBG" }, new String[] { "visualbasic", "VB" } },
-            { new String[]{ ".LOG" }, new String[] { "log", Translates.LogDocument(Settings.Default.translates) } },
-            { new String[]{ ".VSCT" }, new String[] { "vs2019", Translates.LogDocument(Settings.Default.translates) } }
+            { new String[] { ".CSHTML", ".RAZOR" }, new String[] { "cshtml", "CSHtml" } },
+            { new String[] { ".RS" }, new String[] { "rust", "Rust" } },
+            { new String[] { ".TOML" }, new String[] { "toml", "TOML" } },
+            { new String[] { ".LUA" }, new String[] { "lua", "Lua" } },
+            { new String[] { ".BAS", ".CLS", ".FRM", ".VBP", ".VBG" }, new String[] { "visualbasic", "VB" } },
+            { new String[] { ".LOG" }, new String[] { "log", Translates.LogDocument(Settings.Default.translates) } },
+            { new String[] { ".VSCT" }, new String[] { "vs2019", Translates.LogDocument(Settings.Default.translates) } }
         };
         public static readonly Dictionary<Int32, String> IdeVersions = new Dictionary<Int32, String>
         {
             { 15, "2017" },
-            { 16, "2019" }
+            { 16, "2019" },
+            { 17, "2022" }
         };
     }
 }
